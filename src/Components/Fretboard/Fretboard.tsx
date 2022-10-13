905a295bc8d0f6f4dad148bfb12987a82d2a4b17
EricChukoltyansky <kazohelmet@gmail.com>
2022-10-13 16:06:45 +0300

    strings are done

--- src/Components/Fretboard/Fretboard.tsx
+++ src/Components/Fretboard/Fretboard.tsx
@@ -1,13 +1,118 @@
 import React from "react";
 import styled from "styled-components";
 
-const Frtboard = styled.div``;
-const String = styled.div``;
+const Frtboard = styled.div`
+  display: flex;
+  flex-direction: column;
+  background: #be975b;
+  width: 100%;
+  height: 302px;
+  margin-top: 50px;
+`;
+const String = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  position: relative;
+
+  &::before {
+    content: "";
+    width: 100%;
+    height: 10px;
+    background: linear-gradient(#eee, #999);
+    box-shadow: 76px;
+  }
+`;
 const Note = styled.div``;
 
 function Fretboard() {
   return (
     <Frtboard>
+      <String>
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+      </String>
+      <String>
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+      </String>
+      <String>
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+      </String>
+      <String>
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+      </String>
+      <String>
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+      </String>
       <String>
         <Note />
         <Note />
