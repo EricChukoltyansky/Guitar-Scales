4858fb2bcb065e4f234ea889c4342e289f53862b
EricChukoltyansky <kazohelmet@gmail.com>
2022-10-23 13:17:26 +0300

    24 frets dynamic guitar

--- src/Components/Fretboard/Fretboard.tsx
+++ src/Components/Fretboard/Fretboard.tsx
@@ -93,7 +93,7 @@ const Note = styled.div`
 `;
 
 function Fretboard() {
-  const [frets, setFrets] = React.useState(14);
+  const [frets, setFrets] = React.useState(25);
   const [strings, setStrings] = React.useState(6);
   return (
     <Frtboard>
@@ -106,6 +106,11 @@ function Fretboard() {
                 {i === 5 && <div className="single-fretmark"></div>}
                 {i === 7 && <div className="single-fretmark"></div>}
                 {i === 12 && <div className="double-fretmark"></div>}
+                {i === 15 && <div className="single-fretmark"></div>}
+                {i === 17 && <div className="single-fretmark"></div>}
+                {i === 19 && <div className="single-fretmark"></div>}
+                {i === 22 && <div className="single-fretmark"></div>}
+                {i === 24 && <div className="double-fretmark"></div>}
               </Note>
             ))}
           {i !== 0 && Array.from({ length: frets }, (_, i) => <Note></Note>)}
